3d5c738a89531ceae84b7c8a0fcf4380ba326022
Sven Hager <hager.sven@googlemail.com>
2014-05-22 14:00:26 +0200

    add whitespace

--- src/emit.cpp
+++ src/emit.cpp
@@ -104,6 +104,6 @@ std::string Emitter::num_to_ip(const dim_t ip_num) {
   ss  << ((ip_num & 0xFF000000) >> 24) << "."
       << ((ip_num & 0x00FF0000) >> 16) << "."
       << ((ip_num & 0x0000FF00) >>  8) << "."
-      << (ip_num & 0x000000FF);
+      <<  (ip_num & 0x000000FF);
   return ss.str();
 }
