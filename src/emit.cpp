e20bbb5a15d0cf73231d13db15e921531fe371fb
Sven Hager <hager.sven@googlemail.com>
2014-05-24 17:24:21 +0200

    some further work on emitting iptables rulesets

--- src/emit.cpp
+++ src/emit.cpp
@@ -34,26 +34,23 @@ void Emitter::emit_tree_linear_search(TreeNode* tree,
     const std::string& chain, const size_t tree_id,
     const std::string& next_chain, std::stringstream& out) {
 
-  size_t chain_count = 0;
   std::string start_chain(Emitter::build_chain_name(chain, tree_id,
-      chain_count++));
+      tree->id()));
   out << "# Tree " << tree_id << " for Chain " << chain << std::endl;
   out << "-A " << chain << " -j " << start_chain << std::endl;
   NodeRefQueue node_fifo;
   node_fifo.push(tree);
 
   while (!node_fifo.empty()) {
-    TreeNode* current_node = node_fifo.front();
+    TreeNode* node = node_fifo.front();
     node_fifo.pop();
-    if (current_node->is_leaf())
-      emit_leaf(current_node,
-          Emitter::build_chain_name(chain, tree_id, ++chain_count), next_chain,
-          out);
+    if (node->is_leaf())
+      emit_leaf(node, Emitter::build_chain_name(chain, tree_id, node->id()),
+          next_chain, out);
     else {
-      emit_simple_linear_dispatch(current_node, chain, tree_id, ++chain_count,
-          out);
+      emit_simple_linear_dispatch(node, chain, tree_id, node->id(), out);
       // now ensure that all children of this node are traversed
-      NodeVector& children = current_node->children();
+      NodeVector& children = node->children();
       const size_t num_children = children.size();
       for (size_t i = 0; i < num_children; ++i)
         node_fifo.push(&children[i]);
@@ -75,13 +72,13 @@ void emit_linear_ip_dispatch(TreeNode* node,
   NodeVector& children = node->children();
   const size_t num_children = children.size();
   for (size_t i = 0; i < num_children; ++i) {
-    const DimTuple& range = children[i].box().box_bounds()[dim];
-    out << "-A " << Emitter::build_chain_name(chain, tree_id,
-        chain_count + i + 1)
+    TreeNode& child = children[i];
+    const DimTuple& range = child.box().box_bounds()[dim];
+    out << "-A " << Emitter::build_chain_name(chain, tree_id, chain_count)
         << " -m iprange --" << flag << "-range "
         << Emitter::num_to_ip(std::get<0>(range)) << "-"
         << Emitter::num_to_ip(std::get<1>(range)) << " -j "
-        << Emitter::build_chain_name(chain, tree_id, chain_count + i + 1)
+        << Emitter::build_chain_name(chain, tree_id, child.id())
         << std::endl;
   }
   out << std::endl;
@@ -100,14 +97,15 @@ void emit_linear_port_dispatch(TreeNode* node,
   NodeVector& children = node->children();
   const size_t num_children = children.size();
   for (size_t i = 0; i < num_children; ++i) {
-    const DimTuple& range = children[i].box().box_bounds()[dim];
-    out << "-A " << Emitter::build_chain_name(chain, tree_id,
-        chain_count + i + 1)
+    TreeNode& child = children[i];
+    const DimTuple& range = child.box().box_bounds()[dim];
+    out << "-A " << Emitter::build_chain_name(chain, tree_id, chain_count)
         << " -p " << node->prot() << " --" << flag << " "
         << std::get<0>(range) << ":" << std::get<1>(range) << " -j "
-        << Emitter::build_chain_name(chain, tree_id, chain_count + i + 1)
+        << Emitter::build_chain_name(chain, tree_id, child.id())
         << std::endl;
   }
+  out << std::endl;
 }
 
 
@@ -123,24 +121,26 @@ void Emitter::emit_simple_linear_dispatch(TreeNode* node,
     switch (cut_dim) {
       // src port
       case 0:
-        emit_linear_port_dispatch(child, chain, tree_id, chain_count, "sport", cut_dim, out);
+        emit_linear_port_dispatch(child, chain, tree_id, chain_count, "sport",
+            cut_dim, out);
         break;
       // dst port
       case 1:
-        emit_linear_port_dispatch(child, chain, tree_id, chain_count, "dport", cut_dim, out);
+        emit_linear_port_dispatch(child, chain, tree_id, chain_count, "dport",
+            cut_dim, out);
         break;
       // src address
       case 2:
-        emit_linear_ip_dispatch(child, chain, tree_id, chain_count, "src", cut_dim, out);
+        emit_linear_ip_dispatch(child, chain, tree_id, chain_count, "src",
+            cut_dim, out);
         break;
       // dst address
       case 3:
-        emit_linear_ip_dispatch(child, chain, tree_id, chain_count, "dst", cut_dim, out);
+        emit_linear_ip_dispatch(child, chain, tree_id, chain_count, "dst",
+            cut_dim, out);
     }
   }
 }
-
-
 
 
 std::string Emitter::build_chain_name(const std::string& chain,
