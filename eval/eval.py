67898b327af4976326d93ba990ad5f6dee193876
Sven Hager <hager.sven@googlemail.com>
2014-05-31 19:44:06 +0200

    use classbench only for trace generation

--- eval/eval.py
+++ eval/eval.py
@@ -5,10 +5,10 @@ import random
 import time
 
 IP = "141.20.33.253"
-WAIT_SECONDS = 5
+WAIT_SECONDS = 3
 
 def usage_and_exit(prog):
-    print "\nUsage: python %s <NUM PACKETS>\n" % prog
+    print "\nUsage: python %s <TIMEOUT>\n" % prog
     sys.exit(1)
 
 
@@ -23,27 +23,31 @@ def fetch_remote_udp_count():
     return int(content)
 
 
+def send_udp_trace(trace_file, timeout):
+    cmd = "sudo timeout %ds ./sender %s yes" % (timeout, trace_file)
+    os.system(cmd)
+
 def main(argv):
     try:
-        num_packets = int(argv[1])
+        timeout = int(argv[1])
     except:
         usage_and_exit(argv[0])
 
-    print "UDP count before sending %d packets: %d" % (num_packets,
-            fetch_remote_udp_count())
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    for _ in range(num_packets):
-        port = random.randrange(65536)
-        sock.sendto("", (IP, port))
+    start_count = fetch_remote_udp_count()
+    #####sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #####for _ in range(num_packets):
+    #####    port = random.randrange(65536)
+    #####    sock.sendto("", (IP, port))
 
     # wait some time in order to get the PI process the pending packets
-    print "Sent %d packets, now wait for %d seconds ..." % (num_packets,
-            WAIT_SECONDS)
-    #time.sleep(5)
+    ##print "Sent %d packets, now wait for %d seconds ..." % (num_packets,
+    ##        WAIT_SECONDS)
 
-    print "UDP count after sending %d packets: %d" % (num_packets,
-            fetch_remote_udp_count())
+    send_udp_trace("rules_trace", timeout)
+
+    time.sleep(WAIT_SECONDS)
+    end_count = fetch_remote_udp_count()
+    print "Pi received %d UDP packets" % (end_count - start_count)
 
 if __name__ == "__main__":
     main(sys.argv)
